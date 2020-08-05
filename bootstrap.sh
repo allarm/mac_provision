4644bb008731a3a1e898740b1f6641a9fcf6afb8
Andrey Che <andrey.chertolyas@gmail.com>
2020-08-05 16:45:34 +0800

    Fixed

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,12 +13,13 @@ else
     echo "XCode CLT is installed."
 fi
 
-if test ! $(which brew); then
+if ! command -v brew &> /dev/null
+then
     echo "Installing homebrew..."
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    echo "Homebrew is installed."
 fi
-
-brew update
 
 echo "Installing Python $PYTHON_VERSION..."
 
